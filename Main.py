8d9f71d6b456eaa7f09a490f52bd0d8d1fa2feb0
CodeAndMoor <grantjdowns@gmail.com>
2023-09-06 21:20:37 -0500

    Logic Finished

--- Main.py
+++ Main.py
@@ -1,27 +1,40 @@
-from GetWord import GetWord
+from GetWord     import GetWord
+from CheckGuess  import CheckGuess
+from GuessLength import CheckGuessLength
 
-#Get word for this play
-TargetWord = GetWord()
+WantsToPlay = True
 
-#Handle API Errors
-if TargetWord == "Error":
-    print("The API seems to be down. Please try again later")
+while WantsToPlay == True:
+    #Get word for this play
+    TargetWord = GetWord()
 
-UserGuessing = True
+    #Handle API Errors
+    if TargetWord == "Error":
+            print("The API seems to be down. Please try again later")
 
-#Guessing Loop
-while UserGuessing == True:
-    guess = input("Enter your guess")
-    
-    #Determine if word is 5 letters long
-    if len(guess) != 5:
-        print("That guess is not 5 letters. Try again")
+    #Set Number of Guesses to 0
+    UserAttempts = 0
+
+    #Main Loop while playing the game
+    while UserAttempts <= 6:
+        guess = CheckGuessLength()             
+
+        #Win Condition
+        if guess == TargetWord:
+            print(guess)
+            print("You win!") 
+        #Check Guess Letters
+        else:
+            results = CheckGuess(guess, TargetWord)
+            print(results)
+            UserAttempts = UserAttempts + 1
+
+    if UserAttempts > 6:
+        print("Sorry you lost. The word was " + TargetWord + ".")
+
+    PlayAgainString = input("Would you like to play again? ('y' for yes)")
+    if PlayAgainString.lower == "y":
+        WantsToPlay = True
     else:
-        UserGuessing = False
-
-#Initialize Guess Results for Check Loop
-GuessResults = ""
-
-#Check Letters Loop
-for i in range(5):
-    if guess[i] == TargetWord[i]:
+        WantsToPlay = False
+        print("Thanks for playing!")
